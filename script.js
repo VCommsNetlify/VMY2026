54e65e69c88f4a99ccb884b1f8159d0fd3a57d77
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2026-06-16 23:43:05 +0800

    added day 0 registration gallery

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const translations = {
         
         // Gallery Section
         
-        "gallery.reg": "REGISTRATION",
+        "gallery.reg": "REGISTRATION DAY",
         "gallery.day1": "DAY 1",
         "gallery.day2": "DAY 2",
         "gallery.day3": "DAY 3",
@@ -1317,7 +1317,7 @@ const galleryBase = "assets/gallery/";
 
 // Example: Day 1 has 40 images
 const galleryData = {
-    "reg": { folder: "day0", count: 25 },
+    "reg": { folder: "day0", count: 43 },
     "day1": { folder: "day1", count: 40 },
     "day2": { folder: "day2", count: 50 },
     "day3": { folder: "day3", count: 45 },
