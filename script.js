80d4986bdf0bef690bb6af9552a66359143236ce
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2026-06-21 00:37:06 +0800

    gallery day 2

--- script.js
+++ script.js
@@ -1320,7 +1320,7 @@ const galleryBase = "assets/gallery/";
 const galleryData = {
     "reg": { folder: "day0", count: 43 },
     "day1": { folder: "day1", count: 58 },
-    "day2": { folder: "day2", count: 50 },
+    "day2": { folder: "day2", count: 155 },
     "day3": { folder: "day3", count: 45 },
     "day4": { folder: "day4", count: 35 },
     "day5": { folder: "day5", count: 30 },
